a28b5c4afe41227e0fc07ba8b967220074bb2e2c
ttpf-P <65406575+ttpf-P@users.noreply.github.com>
2020-10-27 09:37:59 +0100

    added header handling

--- server.py
+++ server.py
@@ -27,10 +27,14 @@ def accept(sock):
     conn, addr = sock.accept()
     print("addr:"+str(addr))
     conn.setblocking(False)
-    data_cont=types.SimpleNamespace(auth="NOAUTH",ID=ID)#creating data container
+    data_cont=types.SimpleNamespace(auth="NOAUTH",ID=ID,message_len=0,message_type=b"",message=b"",header=b"")#creating data container
     addr_list[ID]=(conn,data_cont)
     ID+=1
     sel.register(conn, events, data=data_cont)#register connection with data container @ selector
+
+
+def handle_tcp_data(data_processed):
+    print(data_processed)
 
 def black_magic(key, mask):
     """main tcp handler"""
@@ -40,14 +44,44 @@ def black_magic(key, mask):
     
     if mask & selectors.EVENT_READ: #connection is readable
         data = conn.recv(1024)
-        
-        datalen = data[:8]
-        datatype = data[8:16]
+        while data!=b"":
+            if key.data.message_len==0:
+                if len(data)<16 and header==b"":
+                    header=data
+                elif header!=b"":
+                    if data+header<16:
+                        header+=data
+                    else:
+                        data=header+data
+                        datalen = data[:8]
+                        key.data.message_len=int(datalen)
+                        datatype = data[8:16]
+                        key.data.message_type=datatype
+                        data=data[16:]
+                else:
+                    datalen = data[:8]
+                    key.data.message_len=int(datalen)
+                    datatype = data[8:16]
+                    key.data.message_type=datatype
+                    data=data[16:]
+            if key.data.message_len==0 and datatype == b"" and not datalen == b"":#bad health, closing connection
+                print("closing connection "+ str(key.data.ID)+" due to bad health")
+                data==b""
+            readlen=len(data)
+            if readlen<=key.data.message_len:
+                key.data.message+=data[:key.data.message_len]
+                key.data.message_len=0
+                handle_tcp_data(key.data.message)
+                key.data.message=b""
+                data=data[key.data.message_len:]
+            else:
+                key.data.message+=data
+                key.data.message_len-=readlen
+                data=b""
+            
 
         print(datalen,datatype)
-        if datatype == b"" and not datalen == b"":#bad health, closing connection
-            print("closing connection "+ str(key.data.ID)+" due to bad health")
-            data==b""
+        
         #print(str(data))
         if data==b"":
             sel.unregister(conn)
